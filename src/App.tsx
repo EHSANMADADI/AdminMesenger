2a05c3f2bb9cc1ce373223aad4a1c12febfbf260
Ehsanmadadi <ehsanmadadialengareqhi@gmail.com>
2024-10-09 08:20:28 +0330

    add Loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NotFound from "./Component/NotFound";
@@ -10,21 +10,36 @@ import AddGroupPage from "./pages/AddGroupPage";
 import UserEditPage from "./pages/UserEditPage";
 import SearchPage from "./pages/SearchPage";
 import AddPermissionPage from "./pages/AddPermissionPage";
+import Loader from "./Component/Loader";
 
 function App() {
+  const LoginPage = lazy(() => import("./pages/LoginPage"));
+  const AdminFerstPage = lazy(() => import("./pages/AdminFerstPage"));
+  const AddNewUserPage = lazy(() => import("./pages/AddNewUserPage"));
+  const ManageUserPage = lazy(() => import("./pages/ManageUserPage"));
+  const AddGroupPage = lazy(() => import("./pages/AddGroupPage"));
+  const UserEditPage = lazy(() => import("./pages/UserEditPage"));
+  const SearchPage = lazy(() => import("./pages/SearchPage"));
+  const AddPermissionPage = lazy(() => import("./pages/AddPermissionPage"));
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="Admin" element={<AdminFerstPage />} />
-        <Route path="Admin/AddNewUser" element={<AddNewUserPage />} />
-        <Route path="Admin/ManageUser" element={<ManageUserPage />} />
-        <Route path="Admin/AddGroup" element={<AddGroupPage />} />
-        <Route path="Admin/AddPermission" element={<AddPermissionPage />} />
-        <Route path="Admin/ManageUser/edit/:userId" element={<UserEditPage />} />
-        <Route path="/Admin/Search" element={<SearchPage/>}/>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="Admin" element={<AdminFerstPage />} />
+          <Route path="Admin/AddNewUser" element={<AddNewUserPage />} />
+          <Route path="Admin/ManageUser" element={<ManageUserPage />} />
+          <Route path="Admin/AddGroup" element={<AddGroupPage />} />
+          <Route path="Admin/AddPermission" element={<AddPermissionPage />} />
+          <Route
+            path="Admin/ManageUser/edit/:userId"
+            element={<UserEditPage />}
+          />
+          <Route path="/Admin/Search" element={<SearchPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
---
+++ src/Component/Loader.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
+import React from 'react'
+import Loading from '../Image/loader/loader.gif'
+export default function Loader() {
+  return (
+    <div className='flex w-full h-full justify-center items-center  h-screen'>
+        <img src={Loading} className='bg-transparent'/> 
+
+    </div>
+  )
+}
